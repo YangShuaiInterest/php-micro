351ec90bbe19b28789ee18511f08fec4e8185a11
james <shuai.yang@ifchange.com>
2019-01-04 20:18:59 +0800

    delete .idea

---
+++ app/Views/index.php
@@ -0,0 +1,5 @@
+<h2>视图展示</h2>
+<?php
+echo $res->name;
+
+echo session_id();
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 use \NoahBuscher\Macaw\Macaw;
 
 Macaw::get('/', function () {
-    var_dump(new \App\Controllers\Users());die;
+
 });
 
 Macaw::get('/users', 'App\Controllers\UsersController@index');
